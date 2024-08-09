aaa4bc4fb5e83f5d4235f5b8e91d5aa38cb9c063
Monsler <105060825+Monsler@users.noreply.github.com>
2024-08-10 00:39:24 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,13 +2,9 @@ local term = require 'term'
 local computer = require 'computer'
 local keyboard = require 'keyboard'
 local thread = require 'thread'
+local shell = require 'shell'
 
 term.clear()
 
 print("Welcome to FlatOS installer!\n\nCurrent uptime: "..computer.uptime()..'\nTo cancel installation, press q.\n')
 
-thread.create(function()
-  while true do
-    if keyboard.isKeyDown('q') then break end
-  end
-end)
